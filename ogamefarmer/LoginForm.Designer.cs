8ce5e2831fb10d9e2e39b9e77ef0ce3a277ef324
bxu <bxu@cybozu.net.cn>
2013-08-29 10:16:09 +0800

    c

---
+++ ogamefarmer/LoginForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace OgameFarmer
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_pw = new System.Windows.Forms.TextBox();
+            this.b_login = new System.Windows.Forms.Button();
+            this.cb_uni = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.l_loginMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(180, 48);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(100, 21);
+            this.tb_username.TabIndex = 2;
+            // 
+            // tb_pw
+            // 
+            this.tb_pw.Location = new System.Drawing.Point(180, 75);
+            this.tb_pw.Name = "tb_pw";
+            this.tb_pw.Size = new System.Drawing.Size(100, 21);
+            this.tb_pw.TabIndex = 3;
+            // 
+            // b_login
+            // 
+            this.b_login.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.b_login.Location = new System.Drawing.Point(54, 124);
+            this.b_login.Name = "b_login";
+            this.b_login.Size = new System.Drawing.Size(200, 57);
+            this.b_login.TabIndex = 4;
+            this.b_login.Text = "Login";
+            this.b_login.UseVisualStyleBackColor = true;
+            this.b_login.Click += new System.EventHandler(this.b_login_Click);
+            // 
+            // cb_uni
+            // 
+            this.cb_uni.FormattingEnabled = true;
+            this.cb_uni.Items.AddRange(new object[] {
+            "u7",
+            "u8"});
+            this.cb_uni.Location = new System.Drawing.Point(35, 75);
+            this.cb_uni.Name = "cb_uni";
+            this.cb_uni.Size = new System.Drawing.Size(82, 20);
+            this.cb_uni.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "请选择宇宙";
+            // 
+            // l_loginMessage
+            // 
+            this.l_loginMessage.AutoSize = true;
+            this.l_loginMessage.Location = new System.Drawing.Point(33, 202);
+            this.l_loginMessage.Name = "l_loginMessage";
+            this.l_loginMessage.Size = new System.Drawing.Size(0, 12);
+            this.l_loginMessage.TabIndex = 31;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 257);
+            this.Controls.Add(this.l_loginMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_uni);
+            this.Controls.Add(this.b_login);
+            this.Controls.Add(this.tb_pw);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Ogame 快乐的农夫-登录";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_pw;
+        private System.Windows.Forms.Button b_login;
+        private System.Windows.Forms.ComboBox cb_uni;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label l_loginMessage;
+    }
+}
---
+++ ogamefarmer/LoginForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace OgameFarmer
+{
+    public partial class LoginForm : Form
+    {
+        private StarScript ss;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.ss = new StarScript();
+            ss.Osender += this.OnLogin;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            cb_uni.DropDownStyle = ComboBoxStyle.DropDownList;
+            cb_uni.SelectedIndex = 1;
+        }
+
+        private void b_login_Click(object sender, EventArgs e)
+        {
+            login();
+            this.b_login.Text = "login...";
+            this.b_login.Enabled = false;
+        }
+
+        private void login()
+        {
+            StarScript.loginname = this.tb_username.Text;
+            StarScript.password = this.tb_pw.Text;
+            StarScript.universe = cb_uni.SelectedItem.ToString();
+            try
+            {
+                ss.run(2);
+            }
+            catch (Exception ee)
+            {
+                ee.ToString();
+            }
+        }
+
+        private LoginInfo li;
+
+        private void OnLogin(object o)
+        {
+            if (o.GetType() == typeof(LoginInfo))
+            {
+                li = (LoginInfo)o;
+                Object[] list = { this, System.EventArgs.Empty };
+                this.l_loginMessage.BeginInvoke(new EventHandler(ShowMessage), list);
+            }
+        }
+
+        public void ShowMessage(object sender, EventArgs e)
+        {
+            if (li.LoginSuccess)
+            {
+                Thread.Sleep(2000);
+                Main m = new Main(this.ss);
+                m.Show();
+                ss.Osender -= this.OnLogin;
+                this.Hide();
+            }
+            else
+            {
+                b_login.Text = "Login";
+                b_login.Enabled = true;
+                l_loginMessage.Text = "登录失败";
+            }
+        }
+    }
+}
