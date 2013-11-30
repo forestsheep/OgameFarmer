3ea3a2f948d0e314b71901d4e66fd6cae1c4812e
Forest Sheep <forestsheep@163.com>
2013-11-30 18:57:17 +0800

    #11

--- ogamefarmer/strategy/AllDefence.cs
+++ ogamefarmer/strategy/AllDefence.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace OgameFarmer
 {
     class AllDefence
     {
+        internal const int MAX_REQUEST_NUMBER = 5000;
         internal static int Metal;
         internal static int Crystal;
         internal static int HH;
@@ -61,30 +63,64 @@ namespace OgameFarmer
 
             ha.AccessUrl = "http://u8.cicihappy.com/ogame/buildings.php?mode=defense";
             ha.AccessMethod = HttpAccesser.ACCESS_METHOD.POST;
-            ha.UrlParam = "fmenge[401]=" + fmenge401 + "&fmenge[402]=" + fmenge402 + "&fmenge[403]=0&fmenge[404]=0&fmenge[405]=" + fmenge405 + "&fmenge[406]=" + fmenge406 + "&fmenge[502]=0&fmenge[503]=0";
-            ha.access();
+            doAccess(ha, fmenge401, fmenge402, fmenge405, fmenge406);
         }
 
-        private static void doAccess(int f401, int f402, int f405, int f406)
+        private static void doAccess(HttpAccesser ha, int f401, int f402, int f405, int f406)
         {
+            int fmenge401 = 0;
+            int fmenge402 = 0;
+            int fmenge405 = 0;
+            int fmenge406 = 0;
+            if (f401 > MAX_REQUEST_NUMBER)
+            {
+                fmenge401 = MAX_REQUEST_NUMBER;
+            }
+            else
+            {
+                fmenge401 = f401;
+            }
+            if (f402 > MAX_REQUEST_NUMBER)
+            {
+                fmenge402 = MAX_REQUEST_NUMBER;
+            }
+            else
+            {
+                fmenge402 = f402;
+            }
+            if (f405 > MAX_REQUEST_NUMBER)
+            {
+                fmenge405 = MAX_REQUEST_NUMBER;
+            }
+            else
+            {
+                fmenge405 = f405;
+            }
+            if (f406 > MAX_REQUEST_NUMBER)
+            {
+                fmenge406 = MAX_REQUEST_NUMBER;
+            }
+            else
+            {
+                fmenge406 = f406;
+            }
 
-            if (f401 > 5000)
-            {
-                // do access f401 = 5000
-            }
-            if (f402 > 5000)
-            {
-                // do access f402 = 5000
-            }
-            // ha.access();
-            f401 -= 5000;
+            f401 -= fmenge401;
+            f402 -= fmenge402;
+            f405 -= fmenge405;
+            f406 -= fmenge406;
+
+            ha.UrlParam = "fmenge[401]=" + fmenge401 + "&fmenge[402]=" + fmenge402 + "&fmenge[403]=0&fmenge[404]=0&fmenge[405]=" + fmenge405 + "&fmenge[406]=" + fmenge406 + "&fmenge[502]=0&fmenge[503]=0";
+            ha.access();
+            Thread.Sleep(3000);
+            
             if (f401 == 0 && f402 == 0 && f405 == 0 && f406 == 0)
             {
                 return;
             }
             else
             {
-                doAccess(f401, f402, f405, f406);
+                doAccess(ha, f401, f402, f405, f406);
             }
         }
     }
