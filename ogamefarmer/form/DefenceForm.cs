5950be9492994edc2652a5c6cd60b63eb2b456ca
Forest Sheep <forestsheep@163.com>
2013-12-11 21:54:55 +0800

    #12

---
+++ ogamefarmer/form/DefenceForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OgameFarmer.form
+{
+    public partial class DefenceForm : Form
+    {
+        public DefenceForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
